d13991f090f71fe3c935cf298578b98b7eaf7608
Thomas Metais <metais.thomas92@gmail.com>
2026-01-28 17:12:33 +0100

    ajout tests pour Rectangle, Cercle & Carre

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "Point.h"
 #include "Forme.h"
+#include "Cercle.h"
+#include "Rectangle.h"
+#include "Carre.h"
 
 #include <iostream>
 
@@ -9,6 +12,18 @@ int main(int argc, char **argv)
   Point p2(4.0, 5.0);
   p1 += p2;
   std::cout << "Point 1 : " << p1 << std::endl;
+  Forme *r1 = new Rectangle(p1, 54, 34);
+  std::cout << *static_cast<Rectangle *>(r1) << std::endl;
+
+  Forme *c1 = new Cercle(Point(5.0, 6.0), 10.0);
+  std::cout << *static_cast<Cercle *>(c1) << std::endl;
+
+  Forme *carre1 = new Carre(Point(1.0, 1.0), 5.0);
+  std::cout << *static_cast<Carre *>(carre1) << std::endl;
+
+  delete r1;
+  delete c1;
+  delete carre1;
 
   return 0;
 }
