44f971874d5bbebd528f2a8afa0445e1c836f494
Thomas Metais <metais.thomas92@gmail.com>
2026-01-29 23:48:40 +0100

    tests complets

--- main.cpp
+++ main.cpp
@@ -3,12 +3,52 @@
 #include "Cercle.h"
 #include "Rectangle.h"
 #include "Carre.h"
+#include "ListeFormes.h"
 
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+
+// fonction pour générer des formes aléatoires
+void ajouterFormesAleatoires(std::vector<Forme *> &formes, int nombre)
+{
+  for (int i = 0; i < nombre; i++)
+  {
+    // position aléatoire entre -50 et 50
+    double x = (rand() % 100) - 50;
+    double y = (rand() % 100) - 50;
+    Point centre(x, y);
+
+    // type de forme aléatoire (0=Cercle, 1=Rectangle, 2=Carré)
+    int type = rand() % 3;
+
+    if (type == 0)
+    {
+      // cercle avec rayon entre 5 et 20
+      double rayon = 5 + (rand() % 16);
+      formes.push_back(new Cercle(centre, rayon));
+    }
+    else if (type == 1)
+    {
+      // rectangle avec dimensions entre 10 et 40
+      double largeur = 10 + (rand() % 31);
+      double hauteur = 10 + (rand() % 31);
+      formes.push_back(new Rectangle(centre, largeur, hauteur));
+    }
+    else
+    {
+      // carré avec côté entre 5 et 25
+      double cote = 5 + (rand() % 21);
+      formes.push_back(new Carre(centre, cote));
+    }
+  }
+}
 
 int main(int argc, char **argv)
 {
+  srand(time(NULL));
+
   Point p1(2.0, 3.0);
   Point p2(4.0, 5.0);
   p1 += p2;
@@ -36,9 +76,16 @@ int main(int argc, char **argv)
 
   std::cout << "Surface totale des formes : " << surfaceTotale << std::endl;
 
-  delete r1;
-  delete c1;
-  delete carre1;
+  ajouterFormesAleatoires(formes, 20);
+  boiteEnglobante *boite = new boiteEnglobante(formes);
+  std::cout << (*boite) << std::endl;
+
+  // TODO : créer tous les destrcuteurs et bien viéder la mémoire
+  for (Forme *f : formes)
+  {
+    delete f;
+  }
+  delete boite;
 
   return 0;
 }
